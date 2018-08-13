d579eb83fb8a6494dba29a08baa2f718098fd70a
Khalid <khalidahmed333@hotmail.com>
2018-08-13 22:58:54 +0200

    Use a stable version of react-native and update dependecies accordingly

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { YellowBox } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import RootNavigation from './navigation/RootNavigation';
 
+// The following line is to temporarily hide a warning caused by
+// https://github.com/facebook/react-native/issues/18868
+// and it must be removed once the offical fix is released
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated'])
+
 export default class App extends Component {
+	componentDidMount() {
+		//SplashScreen.hide()
+	}
+
 	render () {
 		return <RootNavigation />
 	}
