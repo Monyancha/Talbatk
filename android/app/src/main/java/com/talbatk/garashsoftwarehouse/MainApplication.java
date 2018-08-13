ecbbf798361b4bc2d652d3561203eb6afe1fa072
Khalid <khalidahmed333@hotmail.com>
2018-08-14 01:00:06 +0200

    Install and link (Android) react-native-vector-icons package

--- android/app/src/main/java/com/talbatk/garashsoftwarehouse/MainApplication.java
+++ android/app/src/main/java/com/talbatk/garashsoftwarehouse/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-		  new SplashScreenReactPackage()
+		  new SplashScreenReactPackage(),
+		  new VectorIconsPackage()
       );
     }
 
--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default class TestScreen extends Component {
 	render () {
 		return (
-			null
+			<Ionicons name='md-person' size={45} color='purple' /> 
 		);
 	}
 }
