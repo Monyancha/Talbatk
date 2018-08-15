60ee3a0841e87505efe46436d6da824b24675a62
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-08-15 16:17:09 +0200

    pod deleted

--- ios/Pods/Target Support Files/Pods-Talbatk-tvOS/Pods-Talbatk-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Talbatk_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_Talbatk_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-Talbatk-tvOSTests/Pods-Talbatk-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Talbatk_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_Talbatk_tvOSTests
-@end
