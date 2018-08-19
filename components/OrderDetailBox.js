38376c2d9030f7b23e67663d7e5ae77c134a6358
Khalid <khalidahmed333@hotmail.com>
2018-08-20 00:07:49 +0200

    Fix import from react-native-vector-icons errors

--- components/OrderDetailBox.js
+++ components/OrderDetailBox.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
 import Colors from '../constants/Colors';
-import MaterialCommunityIcons from 'react-native-vector-icons'
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export default class OrderDetailBox extends Component {
-
-
-
 	render() {
 		return (
-			<View style={{ flex:1, flexDirection: 'row', justifyContent:'flex-end', alignItems: 'center' }}>
-				<View style={{ flex: 1, paddingTop: 9, paddingRight:10, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-end'}}>
-				<View style={{flex:1,flexDirection:'row'}}>
+			<View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+				<View style={{ flex: 1, paddingTop: 9, paddingRight: 10, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-end' }}>
+					<View style={{ flex: 1, flexDirection: 'row' }}>
 
-				<Text style={{ fontFamily: 'Droid Arabic Kufi', fontSize: 15,textAlign:'right',justifyContent:'flex-end' }}>{this.props.name}</Text>
+						<Text style={{ fontFamily: 'Droid Arabic Kufi', fontSize: 15, textAlign: 'right', justifyContent: 'flex-end' }}>{this.props.name}</Text>
 
 
-				</View>
+					</View>
 					<View style={{ flex: 1 }}>
-						<Text style={{ fontFamily: 'Droid Arabic Kufi', color: '#777777', fontSize: 12,textAlign:'right'}}>{this.props.desc}</Text>
+						<Text style={{ fontFamily: 'Droid Arabic Kufi', color: '#777777', fontSize: 12, textAlign: 'right' }}>{this.props.desc}</Text>
 					</View>
 
 					<View
@@ -87,9 +83,11 @@ export default class OrderDetailBox extends Component {
 				<View style={{ flex: 0.5 }}>
 					<Image
 						source={{ uri: this.props.image }}
-						style={{ width: 100, height: 100, marginTop: 10, marginBottom: 11,
-							marginRight: 4, borderRadius: 10 }}
-							resizeMode="contain"
+						style={{
+							width: 100, height: 100, marginTop: 10, marginBottom: 11,
+							marginRight: 4, borderRadius: 10
+						}}
+						resizeMode="contain"
 					/>
 				</View>
 			</View>
